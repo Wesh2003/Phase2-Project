08a9732e9cd9eead6a96631f1f7b2819ff57f0cf
zakaria <zakimoha2022@gmail.com>
2023-11-22 12:26:15 +0300

    login

---
+++ src/components/Login.js
@@ -0,0 +1,49 @@
+
+import React, { useState } from 'react';
+
+function Login() {
+    // State to hold the input values
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Function to handle form submission
+    const handleLogin = () => {
+        // In a real-world scenario, you would handle the login logic here
+        
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="button" onClick={handleLogin}>
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
