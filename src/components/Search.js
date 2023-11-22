fe00c76e6e2be57fe0a628b25c8b31993bf6437a
christine <christine.muriungi@student.moringaschool.com>
2023-11-22 12:46:39 +0300

    search

---
+++ src/components/Search.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+
+function Search() {
+    const [movies, setMovies] = useState([]);
+    const [searchMovie, setSearchMovie] = useState([]);
+
+useEffect(() => {
+ fetch('https://api.tvmaze.com/schedule')
+   .then((r) => r.json())
+    .then((data) => console.log(data))
+
+
+}, []);
+
+const filteredMovies = movies.filter((movie) =>
+movie.description.toLowerCase().includes(searchMovie.toLowerCase())
+);
+console.log('Filtered Movies:', filteredMovies);
+
+
+
+  return (
+    
+    <div className='Search'>
+      <input 
+      className='mt-5 border-solid border-2 border-gray-300 ...	block flex-1 bg-gray-100 py-1.5 pl-1 text-gray-900 placeholder:text-gray-400  w-full'
+        type='text'
+        placeholder='Search movies by name...'
+        value={searchMovie}
+        onChange={(e) => setSearchMovie(e.target.value)}
+      />
+
+
+
+
+    </div>
+  )
+}
+
+export default Search
