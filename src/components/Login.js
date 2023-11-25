cea7daeb097f126464210971b7465d9dc40c14c8
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-11-26 02:08:45 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,48 @@
+import React, { useState } from 'react';
 
+function Login() {
+    // State to hold the input values
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Function to handle form submission
+    const handleLogin = () => {
+        // In a real-world scenario, you would handle the login logic here
+        e.preventDefault()
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit = {handleLogin}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="button">
+                    Login
+                </button type = "submit">
+            </form>
+        </div>
+    );
+}
+
+export default Login;
